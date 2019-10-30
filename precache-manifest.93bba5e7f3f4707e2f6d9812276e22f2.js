fac9d44512bd3ef0f874fe7307d9724891a2ece4
Viktoryia Dziahtsiaryk <vdziahts@cisco.com>
2019-10-30 17:10:58 +0100

    Updates

---
+++ precache-manifest.93bba5e7f3f4707e2f6d9812276e22f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37f7cf26fb892f871794d9919f495d8e",
+    "url": "/exchange/index.html"
+  },
+  {
+    "revision": "a426e20da279c9019d14",
+    "url": "/exchange/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2925f1f717245d34654f",
+    "url": "/exchange/static/js/2.e7844786.chunk.js"
+  },
+  {
+    "revision": "a426e20da279c9019d14",
+    "url": "/exchange/static/js/main.b90eaacd.chunk.js"
+  },
+  {
+    "revision": "18deb85c4709137261bb",
+    "url": "/exchange/static/js/runtime-main.49c1bb65.js"
+  }
+]);
